8a1dd5c0710015dc8d6014196b24c6ca4cdc8019
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-13 21:32:36 -0500

    6. Half the lies they tell about me aren't true

--- 6-puts2.c
+++ 6-puts2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /*
- *
- * 
- *
+ * puts2 - fuction that prints every other character of a string, starting with the first character.
+ * @char
+ * return nothing. 
 */
 void puts2(char *str){
     int i = 0;
