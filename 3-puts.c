371c6d39ba1e6536a89b26d28b1483d2ce8e22a1
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-13 19:20:58 -0500

    3. I do not fear computers. I fear the lack of them

---
+++ 3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * _puts - function that prints a string.
+ * @char
+ * return nothing.
+*/
+void _puts(char *str){
+    int i = 0; 
+    while(str[i] != '\0'){
+        _putchar(str[i]);
+        i++;
+    }
+    _putchar('\n');
+}
