13df627dcc6daa76d2715e2f190c7d8f0709babd
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-13 22:42:07 -0500

    7. Winning is only half of it. Having fun is the other half

---
+++ 7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+ * puts_half -function that prints half of a string.
+ * @char
+ * return nothing. 
+*/
+void puts_half(char *str){
+    int b, i = 0;    
+    while(str[i] != '\0'){
+        i++;
+    }
+    if(i % 2 == 2){
+        int half = i / 2;
+        while (half <= i){
+            _putchar(str[half]);
+            half++;
+        }
+    } else{
+        int half = (i - 1) / 2; 
+        while(half <= i){
+            _putchar(str[half]);
+            half++;
+        }  
+    }
+    _putchar('\n');
+}
