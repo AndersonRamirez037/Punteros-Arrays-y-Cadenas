a34e82dfedb8c214aca546e253c043bcd88e8ddd
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-13 22:43:10 -0500

    7. Winning is only half of it. Having fun is the other half

--- 7-puts_half.c
+++ 7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str){
     while(str[i] != '\0'){
         i++;
     }
-    if(i % 2 == 2){
+    if(i % 2 == 0){
         int half = i / 2;
         while (half <= i){
             _putchar(str[half]);
